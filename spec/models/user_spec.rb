b00603b0014835bce58e6479419273e4f618f60b
Ryota Ikezawa <luvegr@gmail.com>
2015-11-29 17:29:37 +0900

    Make a basic User model (including secure passwords)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,57 +2,36 @@ require 'spec_helper'
 
 describe User do
 
-  before { @user = User.new(name: "Example User", email: "user@example.com") }
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com", 
+                     password: "foobar", password_confirmation: "foobar")
+  end
+
   subject { @user }
 
-  it 'should respond to name' do
-    expect(@user).to respond_to(:name)
-  end
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) } 
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:authenticate) }
 
-  it 'should respond to email' do
-    expect(@user).to respond_to(:email)
-  end
-
-  it 'should be valid' do
-    expect(@user).to be_valid
-  end
+  it { should be_valid }
 
   describe "when name is not present" do
-    before { @user.name = "  " }
+    before { @user.name = " " }
     it { should_not be_valid }
   end
-
-#  describe "when name is not present" do
-#    before { @user.name = "  " }
-#    it 'should_not be_valid' do
-#      expect(@user.name).not_to be_valid
-#    end
-#  end
 
   describe "when email is not present" do
-    before { @user.email = "  " }
+    before { @user.email = " " }
     it { should_not be_valid }
   end
 
-#  describe "when email is not present" do
-#    before { @user.email = "  " }
-#    it 'should_not be_valid' do
-#      expect(@user.email).not_to be_valid
-#   nd
-
-  describe "when name is too long" do
+  describe "when name is longer than 50 characters" do
     before { @user.name = "a" * 51 }
     it { should_not be_valid }
   end
-
-#  describe "when name is too long" do
-#    before :each do
-#      allow(@user).to receive(:name).and_return("a" * 51)
-#      it 'should not be valid' do
-#        expect(@user.name).not_to be_valid
-#      end
-#    end
-#  end
 
   describe "when email format is invalid" do
     it "should be invalid" do
@@ -70,7 +49,55 @@ describe User do
       addresses.each do |valid_address|
         @user.email = valid_address
         expect(@user).to be_valid
-      end
+      end      
     end
   end
+
+  describe "when email address is already taken" do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save
+    end
+
+    it { should_not be_valid }
+  end
+
+  describe "when password is not present" do
+    before { @user.password = @user.password_confirmation = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
+  end
+
+
+  describe "When password confirmation is nil" do
+    before { @user.password_confirmation = nil }
+    it { should_not be_valid }
+  end
+
+  describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should_not be_valid }
+  end
+
+  describe "return value of authenticate method" do
+    before { @user.save }
+    let(:found_user) { User.find_by(email: @user.email) }
+
+    describe "with valid password" do
+      it { should eq found_user.authenticate(@user.password) }
+    end
+
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+      it { should_not eq user_for_invalid_password }
+      specify { expect(user_for_invalid_password).to be_false }
+    end
+  end
+
 end
