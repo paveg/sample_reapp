f0d7ccd552a2d53d3c368a5fb7a1731680b46d00
Ryota Ikezawa <luvegr@gmail.com>
2015-12-21 02:36:40 +0900

    fix a little

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -14,11 +14,11 @@ describe "Authentication" do
   describe "signin" do
     before { visit signin_path }
 
-    describe "with invalid infromation" do
+    describe "with invalid information" do
       before { click_button "Sign in" }
 
       it { should have_title('Sign in') }
-      it { should have_error_message('Invalid') }
+      it { should have_error_message('invalid') }
 
       describe "after visiting another page" do
         before { click_link "Home" }
