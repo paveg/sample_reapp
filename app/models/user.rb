5204769ceee9fd03c2ca2450ebb0031bad8d4f9a
Ryota Ikezawa <luvegr@gmail.com>
2015-12-29 22:17:07 +0900

    hotfix

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
   before_create :create_remember_token
 
   validates :name,  presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+\.[a-z]+\.[a-z]+\z/i
   validates :email, presence: true,
                       format: { with: VALID_EMAIL_REGEX },
                   uniqueness: { case_sensitive: false }
