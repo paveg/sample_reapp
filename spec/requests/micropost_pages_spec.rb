4df2a5fc8cfeaccc15b33a2a8033a7302c95886b
Ryota Ikezawa <luvegr@gmail.com>
2015-12-30 15:50:20 +0900

    fix little

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
-describe "Micropost pages" do
-
+describe "MicropostPages" do
   subject { page }
 
   let(:user) { FactoryGirl.create(:user) }
@@ -11,7 +10,6 @@ describe "Micropost pages" do
     before { visit root_path }
 
     describe "with invalid information" do
-
       it "should not create a micropost" do
         expect { click_button "Post" }.not_to change(Micropost, :count)
       end
@@ -23,7 +21,6 @@ describe "Micropost pages" do
     end
 
     describe "with valid information" do
-
       before { fill_in 'micropost_content', with: "Lorem ipsum" }
       it "should create a micropost" do
         expect { click_button "Post" }.to change(Micropost, :count).by(1)
